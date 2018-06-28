69a9e539c8bab56caad69c1256820435be5ea8f4
kyle beavin <kylebeavin@gmail.com>
2018-06-27 20:04:03 -0400

    contact form

--- src/components/site/Contact.js
+++ src/components/site/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 class Contact extends Component {
     render() {
@@ -7,6 +8,22 @@ class Contact extends Component {
             <div className="main">
                 <div className="mainDiv">
                     <h1>Contact</h1>
+                    <Form>
+                        <FormGroup>
+                            <Label for="exampleEmail">Email</Label>
+                            <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="examplePassword">Password</Label>
+                            <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="exampleText">Text Area</Label>
+                            <Input type="textarea" name="text" id="exampleText" />
+                        </FormGroup>
+                        <Button>Submit</Button>
+                    </Form>
+
                 </div>
             </div>
         )
