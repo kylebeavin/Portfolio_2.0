f6b751c30b0162cfa8fc4a3abe306af4bea3cf69
kyle beavin <kylebeavin@gmail.com>
2018-07-06 01:54:11 -0400

    contact section styled

--- src/components/content/contact/Contact.js
+++ src/components/content/contact/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Container, Row, Col, Button, Form, FormGroup, Label, Input, Card, CardImg } from 'reactstrap';
+import family1 from '../../../assets/family1.jpg';
 
 class Contact extends Component {
     constructor() {
@@ -15,31 +16,45 @@ class Contact extends Component {
         this.handleChange = this.handleChange.bind(this);
     }
 
-handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value })
-}
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
 
     render() {
         return (
             <div className="main">
                 <div className="mainDiv">
-                    <h1>Contact</h1>
-                    <Form action="https://formspree.io/kylebeavin@gmail.com" method="POST">
-                        <FormGroup>
-                            <Label for="exampleName">Name</Label>
-                            <Input type="text" name="name" id="name" placeholder="Name" onChange={this.handleChange}/>
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="exampleEmail">Email</Label>
-                            <Input type="email" name="email" id="email" placeholder="Email" onChange={this.handleChange}/>
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="exampleText">Message</Label>
-                            <Input type="textarea" name="message" id="exampleText" onChange={this.handleChange}/>
-                        </FormGroup>
-                        <Button>Submit</Button>
-                    </Form>
-
+                    <Container>
+                        <h1>Contact</h1>
+                        <Row>
+                            <Col lg="9" md="6" xs="12">
+                                <Form action="https://formspree.io/kylebeavin@gmail.com" method="POST">
+                                    <FormGroup>
+                                        <Label for="exampleName">Name</Label>
+                                        <Input type="text" name="name" id="name" placeholder="Name" onChange={this.handleChange} />
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Label for="exampleEmail">Email</Label>
+                                        <Input type="email" name="email" id="email" placeholder="Email" onChange={this.handleChange} />
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Label for="exampleText">Message</Label>
+                                        <Input type="textarea" name="message" id="exampleText" onChange={this.handleChange} placeholder="Questions or Comments"/>
+                                    </FormGroup>
+                                    <Button>Submit</Button> 
+                                </Form>
+                            </Col>
+                            <Col>
+                                <Card inverse>
+                                    <CardImg width="100%" src={family1} alt="Card image cap" />
+                                </Card>
+                                <br/>
+                                <h6 style={{ textAlign: "right" }}>Kyle Beavin</h6>
+                                <h6 style={{ textAlign: "right" }}>614-507-9548</h6>
+                                <h6 style={{ textAlign: "right" }}>kylebeavin@gmail.com</h6>
+                            </Col>
+                        </Row>
+                    </Container>
                 </div>
             </div>
         )
