2c7f8491257f0df162360ea6ca3d197a07bad998
kyle beavin <kylebeavin@gmail.com>
2018-07-08 22:45:13 -0400

    glyphs

--- src/components/site/Footer.js
+++ src/components/site/Footer.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
-import { Col, Row } from 'reactstrap';
 
   class Footer extends Component {
   render() {
     return (
       <footer className="footer">
-        <Row>
-          <Col>
           <div className="spacer"></div>
-          <p>Portfolio_2.0</p>
-          </Col>
-          <a href="https://www.linkedin.com/in/kyle-beavin-62758a78" target="blank" className="fab fa-linkedin glyph"></a>
-          <a href="https://github.com/kylebeavin" target="blank" className="fab fa-github glyph"></a>
-          <a href="https://drive.google.com/open?id=1CftfNskaZCpKwZSFqzlsQZwH0Hw94LJj" target="blank" className="far fa-file glyph resume"></a>
-          {/* <i className="fas fa-angle-double-left"></i> */}
-        </Row>
+          <ul className="brandList">
+            <li><a href="https://www.linkedin.com/in/kyle-beavin-62758a78" target="blank"><button><i className="fab fa-linkedin"></i></button></a></li>
+            <li><a href="https://github.com/kylebeavin" target="blank"><button><i className="fab fa-github"></i></button></a></li>
+            <li><a href="https://drive.google.com/open?id=1CftfNskaZCpKwZSFqzlsQZwH0Hw94LJj" target="blank"><button><i className="far fa-file"></i></button></a></li>
+          </ul>
       </footer>
     )
   }
 }
 
+{/* <a href="https://www.linkedin.com/in/kyle-beavin-62758a78" target="blank" className="fab fa-linkedin glyph"></a>
+<a href="https://github.com/kylebeavin" target="blank" className="fab fa-github glyph"></a>
+<a href="https://drive.google.com/open?id=1CftfNskaZCpKwZSFqzlsQZwH0Hw94LJj" target="blank" className="far fa-file glyph resume"></a> */}
 export default Footer;
