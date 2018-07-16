683b3f01c868509cfb6411724b85c6c5748f87cd
kyle beavin <kylebeavin@gmail.com>
2018-07-16 12:38:55 -0400

    fixed back button

--- src/components/site/Footer.js
+++ src/components/site/Footer.js
@@ -6,16 +6,14 @@ import React, { Component } from 'react';
       <footer className="footer">
           <div className="spacer"></div>
           <ul className="brandList">
-            <li><a href="https://www.linkedin.com/in/kyle-beavin-62758a78" target="blank"><button><i className="fab fa-linkedin"></i></button></a></li>
-            <li><a href="https://github.com/kylebeavin" target="blank"><button><i className="fab fa-github"></i></button></a></li>
-            <li><a href="https://drive.google.com/open?id=1CftfNskaZCpKwZSFqzlsQZwH0Hw94LJj" target="blank"><button><i className="far fa-file"></i></button></a></li>
+            <li><a href="https://www.linkedin.com/in/kyle-beavin-62758a78" target="blank"><button className="brandButton"><i className="fab fa-linkedin"></i></button></a></li>
+            <li><a href="https://github.com/kylebeavin" target="blank"><button className="brandButton"><i className="fab fa-github"></i></button></a></li>
+            <li><a href="https://drive.google.com/open?id=1CftfNskaZCpKwZSFqzlsQZwH0Hw94LJj" target="blank"><button className="brandButton"><i className="far fa-file"></i></button></a></li>
+            <li><a href="https://angel.co/matthew-kyle-beavin" target="blank"><button className="brandButton"><i className="fab fa-angellist"></i></button></a></li>
           </ul>
       </footer>
     )
   }
 }
 
-{/* <a href="https://www.linkedin.com/in/kyle-beavin-62758a78" target="blank" className="fab fa-linkedin glyph"></a>
-<a href="https://github.com/kylebeavin" target="blank" className="fab fa-github glyph"></a>
-<a href="https://drive.google.com/open?id=1CftfNskaZCpKwZSFqzlsQZwH0Hw94LJj" target="blank" className="far fa-file glyph resume"></a> */}
 export default Footer;
